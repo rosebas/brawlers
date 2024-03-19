8ccefc71adb0790b8af345f9a465361f06719cab
Sebastian <sebasrosales763@gmail.com>
2024-03-18 23:28:49 -0600

    crea el objeto frank

--- src/main/java/com/umg/Brawlers.java
+++ src/main/java/com/umg/Brawlers.java
@@ -15,9 +15,11 @@ public class Brawlers {
         BrawlClass collete = new BrawlClass("Collete",11, 10, 123, 1, 1);
         BrawlClass crow = new BrawlClass("Crow", 11, 320, 320, 3, 5);
         BrawlClass eve = new BrawlClass("Eve", 11, 540, 1800, 2, 7);
+        BrawlClass frank = new BrawlClass("Frank", 11, 2120, 2120, 3, 5);
         
         System.out.println(crow.nombre + crow.danioBasico);
         System.out.println(collete.nombre + collete.danioBasico);
         System.out.println(eve.nombre + eve.danioBasico);
+        System.out.println(frank.nombre + frank.danioBasico);
     }
 }
