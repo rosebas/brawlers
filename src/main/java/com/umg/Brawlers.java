3d3d84627ed2a600e4bb37072e3723a5a9026c5c
Sebastian <sebasrosales763@gmail.com>
2024-03-18 23:18:56 -0600

    Crea objeto eve

--- src/main/java/com/umg/Brawlers.java
+++ src/main/java/com/umg/Brawlers.java
@@ -1,4 +1,4 @@
-/*
+/* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
@@ -14,8 +14,10 @@ public class Brawlers {
        
         BrawlClass collete = new BrawlClass("Collete",11, 10, 123, 1, 1);
         BrawlClass crow = new BrawlClass("Crow", 11, 320, 320, 3, 5);
+        BrawlClass eve = new BrawlClass("Eve", 11, 540, 1800, 2, 7);
         
         System.out.println(crow.nombre + crow.danioBasico);
         System.out.println(collete.nombre + collete.danioBasico);
+        System.out.println(eve.nombre + eve.danioBasico);
     }
 }
